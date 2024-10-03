f3c969d9bd172c6c2ac0042b2da9ab1146b0fb36
deepanshkhurana <deepanshkhurana@outlook.com>
2024-10-03 17:22:33 +0530

    fix: remove other params from freeform_query

--- R/get_freeform_table_query.R
+++ R/get_freeform_table_query.R
@@ -1,13 +1,11 @@
 #' Query data from a table with a freeform text query
 #'
 #' @param query The custom query as text
-#' @param schema The schema name.
 #' @param conn A database connection object.
 #' @return A data frame with table data.
 #' @export
 get_freeform_table_query <- function(
   query = NULL,
-  schema = Sys.getenv("SUPABASE_SCHEMA"),
   conn = make_connection()
 ) {
   on.exit(DBI::dbDisconnect(conn))
