0bf7b730d327c8d493379f044286ed82bf08eada
deepanshkhurana <deepanshkhurana@outlook.com>
2024-10-03 17:21:03 +0530

    fix: freeform_table_query update

--- R/get_freeform_table_query.R
+++ R/get_freeform_table_query.R
@@ -6,17 +6,12 @@
 #' @return A data frame with table data.
 #' @export
 get_freeform_table_query <- function(
-  table_name = NULL,
   query = NULL,
   schema = Sys.getenv("SUPABASE_SCHEMA"),
   conn = make_connection()
 ) {
   on.exit(DBI::dbDisconnect(conn))
-  checkmate::assert(
-    checkmate::check_string(table_name),
-    checkmate::check_string(query),
-    combine = "and"
-  )
+  checkmate::assert_string(query)
   DBI::dbGetQuery(
     conn,
     query
