9d5465695ba27b8c8ef34e4256c099b2936bf67b
deepanshkhurana <deepanshkhurana@outlook.com>
2026-03-29 03:21:05 +0530

    feat: add sb_db_read for reading table data

---
+++ R/sb_db_read.R
@@ -0,0 +1,33 @@
+#' Read table data
+#'
+#' @param table The table name
+#' @param limit Maximum rows to return (0 for all)
+#' @param schema The schema name
+#' @return A data frame with table data
+#' @export
+sb_db_read <- function(
+  table = NULL,
+  limit = 0,
+  schema = get_schema()
+) {
+  conn <- get_connection()
+  checkmate::assert(
+    checkmate::check_string(table),
+    checkmate::check_numeric(limit),
+    combine = "and"
+  )
+
+  if (!sb_db_table_exists(table, schema)) {
+    stop(glue::glue("Table '{table}' does not exist!"))
+  }
+
+  limit_clause <- if (limit > 0) glue::glue_sql("LIMIT {limit}", .con = conn) else DBI::SQL("")
+
+  DBI::dbGetQuery(
+    conn,
+    glue::glue_sql(
+      "SELECT * FROM {`schema`}.{`table`} {limit_clause}",
+      .con = conn
+    )
+  )
+}
