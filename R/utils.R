9e2ff48bac7289cdf9ff2d0552e8b9fece96e127
deepanshkhurana <deepanshkhurana@outlook.com>
2026-03-29 15:40:50 +0530

    feat: add pkgdown site and unified API wrappers

--- R/utils.R
+++ R/utils.R
@@ -11,7 +11,6 @@
 #' - `list(name = list(like = "A%"))` → `name LIKE 'A%'`
 #'
 #' Supported operators: eq, neq, gt, gte, lt, lte, like, ilike, in, is
-build_where <- function(where, conn, include_keyword = TRUE) {
 build_where <- function(
   where,
   conn,
