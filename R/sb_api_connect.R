cf3f302451853498604b784243bfd4362a61c1ab
Maciej Banas <banasmaciek@gmail.com>
2026-04-02 15:44:50 +0200

    Fix.

--- R/sb_api_connect.R
+++ R/sb_api_connect.R
@@ -6,16 +6,16 @@
 #' @export
 sb_api_connect <- function(
     url = Sys.getenv("SUPABASE_URL"),
-    key = Sys.getenv("SUPABASE_KEY")
+    key = Sys.getenv("SUPABASE_ANON_KEY")
 ) {
   if (url == "" || key == "") {
     stop(
       "API credentials not found.\
-      Set SUPABASE_URL and SUPABASE_KEY."
+      Set SUPABASE_URL and SUPABASE_ANON_KEY."
     )
   }
 
-  .sb_env$api_url <- #url
+  .sb_env$api_url <- url
   .sb_env$api_key <- key
 
   cli::cli_alert_success("API credentials set for {.field {url}}")
